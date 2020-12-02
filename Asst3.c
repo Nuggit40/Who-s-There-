5b7a9e9b58521ece07ca44a386632f2427f9a441
Daniel Teffenhart <dannyteffenhart@gmail.com>
2020-12-02 12:38:59 -0500

    basic communication working

--- Asst3.c
+++ Asst3.c
@@ -1,142 +1,142 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <netdb.h>
 
-void *from_Client(){
-    int sock;
-    struct sockaddr_in server;
-    int mysock;
-    char buffer [1024];
-    int rval;
-
-    //Create Socket
-
-    sock=socket(AF_INET, SOCK_STREAM,0); //makes socket
-    if(sock<0){
-        perror("FAILED to create socket");
-        exit(1);
-    }
-
-             //initializes server info
-    server.sin_family=AF_INET;
-    server.sin_addr.s_addr=INADDR_ANY;
-    server.sin_port = htons(3000);
-
- 
-    //Call bind
-
-    if(bind(sock, (struct sockaddr *)&server, sizeof(server))){
-
-        perror("BIND Failed");
-        exit(1);
-    }
-    //Listen to the Network
-
-    listen(sock, 5);
+#define BACKLOG 5
 
 
+// the argument we will pass to the connection-handler threads
+struct connection {
+    struct sockaddr_storage addr;
+    socklen_t addr_len;
+    int fd;
+};
 
-    //Accept
-
-    do {
-
-        mysock= accept(sock,(struct sockaddr*)0,0);
-        if(mysock==-1){
-            perror("DIDN'T Accept");
-        }else{
-            //initialize buffer
-            memset(buffer,0,sizeof(buffer));
-            if((rval=recv(mysock,buffer,sizeof(buffer),0))<0){
-                perror("READING STREAM MESSAGE ERROR");
-            }else if(rval==0){
-                printf("ENDING CONNECTION\n");
-            }else{
-               printf("MSG: %s\n", buffer);
-            }
-             printf("GOT the message (rval= %d)\n",rval);
-             close(mysock);
-        }
-
-    }while(1);
-}
+void echo(struct connection* arg);
+int server(char *port);
 
 int main(int argc, char **argv)
 {
-	struct addrinfo hints, *address_list, *addr;
-	int error;
-	int sock;
-	int i;
-	
-	if (argc < 4) {
-		printf("Usage: %s [host] [port] [message(s)...]\n", argv[0]);
+	if (argc != 2) {
+		printf("Usage: %s [port]\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
+    (void) server(argv[1]);
+    return EXIT_SUCCESS;
+}
 
-	// we need to provide some additional information to getaddrinfo using hints
-	// we don't know how big hints is, so we use memset to zero out all the fields
-	memset(&hints, 0, sizeof(hints));
-	
-	// indicate that we want any kind of address
-	// in practice, this means we are fine with IPv4 and IPv6 addresses
-	hints.ai_family = AF_UNSPEC;
-	
-	// we want a socket with read/write streams, rather than datagrams
-	hints.ai_socktype = SOCK_STREAM;
 
-	// get a list of all possible ways to connect to the host
-	// argv[1] - the remote host
-	// argv[2] - the service (by name, or a number given as a decimal string)
-	// hints   - our additional requirements
-	// address_list - the list of results
+int server(char *port)
+{
+    struct addrinfo hint, *address_list, *addr;
+    struct connection *con;
+    int error, sfd;
+    // initialize hints
+    memset(&hint, 0, sizeof(struct addrinfo));
+    hint.ai_family = AF_UNSPEC;
+    hint.ai_socktype = SOCK_STREAM;
+    hint.ai_flags = AI_PASSIVE;
+    	// setting AI_PASSIVE means that we want to create a listening socket
 
-	error = getaddrinfo(argv[1], argv[2], &hints, &address_list);
-	if (error) {
-		fprintf(stderr, "%s", gai_strerror(error));
-		exit(EXIT_FAILURE);
-	}
-
-	
-	// try each of the possible connection methods until we succeed
-	for (addr = address_list; addr != NULL; addr = addr->ai_next) {
-		// attempt to create the socket
-		sock = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+    // get socket and address info for listening port
+    // - for a listening socket, give NULL as the host name (because the socket is on
+    //   the local host)
+    error = getaddrinfo(NULL, port, &hint, &address_list);
+    if (error != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(error));
+        return -1;
+    }
+    // attempt to create socket
+    for (addr = address_list; addr != NULL; addr = addr->ai_next) {
+        sfd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+        // if we couldn't create the socket, try the next method
+        if (sfd == -1) {
+            continue;
+        }
+        // if we were able to create the socket, try to set it up for
+        // incoming connections;
+        // 
+        // note that this requires two steps:
+        // - bind associates the socket with the specified port on the local host
+        // - listen sets up a queue for incoming connections and allows us to use accept
+        if ((bind(sfd, addr->ai_addr, addr->ai_addrlen) == 0) &&
+            (listen(sfd, BACKLOG) == 0)) {
+            break;
+        }
+        // unable to set it up, so try the next method
+        close(sfd);
+    }
+    if (addr == NULL) {
+        // we reached the end of result without successfuly binding a socket
+        fprintf(stderr, "Could not bind\n");
+        return -1;
+    }
+    freeaddrinfo(address_list);
+    // at this point sfd is bound and listening
+    printf("Waiting for connection\n");
+    for (;;) {
+    	// create argument struct for child thread
+		con = malloc(sizeof(struct connection));
+        con->addr_len = sizeof(struct sockaddr_storage);
+        	// addr_len is a read/write parameter to accept
+        	// we set the initial value, saying how much space is available
+        	// after the call to accept, this field will contain the actual address length
+        // wait for an incoming connection
+        con->fd = accept(sfd, (struct sockaddr *) &con->addr, &con->addr_len);
+        	// we provide
+        	// sfd - the listening socket
+        	// &con->addr - a location to write the address of the remote host
+        	// &con->addr_len - a location to write the length of the address
+        	//
+        	// accept will block until a remote host tries to connect
+        	// it returns a new socket that can be used to communicate with the remote
+        	// host, and writes the address of the remote hist into the provided location
+        // if we got back -1, it means something went wrong
+        if (con->fd == -1) {
+            perror("accept");
+            continue;
+        }
 		
-		// if we somehow failed, try the next method
-		if (sock < 0) continue;
-		
-		// try to connect to the remote host using the socket
-		if (connect(sock, addr->ai_addr, addr->ai_addrlen) == 0) {
-			// we succeeded, so break out of the loop
-			break;
-		}
+		echo(con);
+    }
 
-		// we weren't able to connect; close the socket and try the next method		
-		close(sock);
-	}
-	
-	// if we exited the loop without opening a socket and connecting, halt
-	if (addr == NULL) {
-		fprintf(stderr, "Could not connect to %s:%s\n", argv[1], argv[2]);
-		exit(EXIT_FAILURE);
-	}
-	
-	// now that we have connected, we don't need the addressinfo list, so free it
-	freeaddrinfo(address_list);
+    // never reach here
+    return 0;
+}
 
-	// send the remaining messages to the remote host
-	for (i = 3; i < argc; ++i) {
-		write(sock, argv[i], strlen(argv[i]));
-		usleep(50 * 1000);
-			// wait 50ms between writes, just to make things interesting
-	}
-	
-	// close the socket
-	close(sock);
-    from_Client();
+void echo(struct connection* arg)
+{
+    char host[100], port[10], buf[101];
+    struct connection *c = (struct connection *) arg;
+    int error, nread;
 
-	return EXIT_SUCCESS;	
+	// find out the name and port of the remote host
+    error = getnameinfo((struct sockaddr *) &c->addr, c->addr_len, host, 100, port, 10, NI_NUMERICSERV);
+    	// we provide:
+    	// the address and its length
+    	// a buffer to write the host name, and its length
+    	// a buffer to write the port (as a string), and its length
+    	// flags, in this case saying that we want the port as a number, not a service name
+    if (error != 0) {
+        fprintf(stderr, "getnameinfo: %s", gai_strerror(error));
+        close(c->fd);
+        return;
+    }
+
+    printf("[%s:%s] connection\n", host, port);
+	//EXCHANGING MESSAGES
+	char* msg = "first message from server!";
+	char buff[256];
+	read(c->fd, buff, 256);
+	printf("read:%s\n", buff);
+	send(c->fd, msg, strlen(msg) + 1, 0);
+	printf("sent:%s\n", msg);
+	read(c->fd, buff, 256);
+	printf("read:%s\n", buff);
+
+    close(c->fd);
+    free(c);
 }
--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 all:
 	gcc Asst3.c -o KKJserver
+	gcc testclient.c -o TC
 clean:
 	rm KKJserver
+	rm TC
